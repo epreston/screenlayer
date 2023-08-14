4472e1770e2b814b40c58435ef3bd4b857797830
Ed Preston <epreston@prestonsoft.com>
2023-08-14 13:57:31 +1000

    Update main.js
    update test page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './css/style.css';
 import viteLogoUrl from './img/vite.svg';
+import { Clock } from 'screenlayer';
+
+const ticker = new Clock();
+
+ticker.start();
 
 document.querySelector('#app').innerHTML = `
   <a href="https://vitejs.dev" target="_blank">
@@ -8,3 +13,7 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite !</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
+
+ticker.stop();
+
+console.log(`Loaded in ${ticker.getElapsedTime()}`);
