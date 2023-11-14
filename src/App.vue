9af54ef84201c7192181b658f607b9dcc96069bf
Ed Preston <epreston@prestonsoft.com>
2023-11-15 03:36:17 +1100

    configure example

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import InboxScreen from './components/InboxScreen.vue';
+</script>
+
+<template>
+  <div id="app">
+    <InboxScreen />
+  </div>
+</template>
+
+<style>
+@import './index.css';
+</style>
