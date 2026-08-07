cc2b73dce12846e5d38334a00a2e5058b0349829
Ed Preston <epreston@prestonsoft.com>
2026-08-08 06:17:02 +1000

    Update vite.config.js
    ensure storybook can resolve project and published aliases to the local version of the components.

--- apps/design/vite.config.js
+++ apps/design/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+import { entries } from '../../scripts/aliases.js';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: entries
+  },
   build: {
     target: ['es2024']
   }
